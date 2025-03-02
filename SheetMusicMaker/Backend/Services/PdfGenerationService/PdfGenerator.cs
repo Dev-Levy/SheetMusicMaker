71bfdad5de1de0c50e80fbc8368e8cdc1197474d
Oláh Levente <levente@stud.uni-obuda.hu>
2025-03-02 22:44:01 +0100

    created service for PDF generation

---
+++ SheetMusicMaker/Backend/Services/PdfGenerationService/PdfGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace PdfGenerationService
+{
+    public static class PdfGenerator
+    {
+        public static void Generate(string musicXmlPath, string outputPdfPath)
+        {
+            string musescoreExePath = @"C:\Program Files\MuseScore 4\bin\MuseScore4.exe";
+
+            ProcessStartInfo startInfo = new()
+            {
+                FileName = musescoreExePath,
+                Arguments = $"\"{musicXmlPath}\" -o \"{outputPdfPath}\"",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            Process.Start(startInfo);
+        }
+    }
+}
