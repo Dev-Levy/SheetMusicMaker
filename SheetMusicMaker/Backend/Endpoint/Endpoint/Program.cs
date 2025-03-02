df578bef7deb26334df81c30f8ffad65bd62aab7
Oláh Levente <levente@stud.uni-obuda.hu>
2025-03-02 23:36:51 +0100

    created controllers

--- SheetMusicMaker/Backend/Endpoint/Endpoint/Program.cs
+++ SheetMusicMaker/Backend/Endpoint/Endpoint/Program.cs
@@ -2,6 +2,7 @@ using BusinessLogic;
 using BusinessLogic.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Models;
 using Repository;
 using Repository.Generics;
@@ -17,15 +18,22 @@ namespace Endpoint
 
             builder.Services.AddControllers();
 
-            builder.Services.AddTransient<SheetMusicMakerDBContext>();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
+            builder.Services.AddTransient<SheetMusicMakerDBContext>();
             builder.Services.AddTransient<IRepository<Recording>, RecordingRepository>();
             builder.Services.AddTransient<IRepository<Pdf>, PdfRepository>();
-
             builder.Services.AddTransient<IPdfLogic, PdfLogic>();
             builder.Services.AddTransient<IRecordingLogic, RecordingLogic>();
 
             var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.UseRouting();
             app.MapControllers();
