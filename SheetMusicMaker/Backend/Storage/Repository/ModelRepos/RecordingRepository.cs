cb1bac91a04ce76dc5199eb9d7582905e3379dee
Oláh Levente <levente@stud.uni-obuda.hu>
2025-03-02 22:07:06 +0100

    created Recording repo

---
+++ SheetMusicMaker/Backend/Storage/Repository/ModelRepos/RecordingRepository.cs
@@ -0,0 +1,22 @@
+﻿using Models;
+using Repository.Generics;
+using System;
+using System.Linq;
+
+namespace Repository.ModelRepos
+{
+    public class RecordingRepository(SheetMusicMakerDBContext ctx) : Repository<Recording>(ctx)
+    {
+        public override Recording Read(int id)
+        {
+            return ctx.Recordings.FirstOrDefault(r => r.Id.Equals(id)) ?? throw new NullReferenceException("No recording found with this Id!");
+        }
+
+        public override void Update(Recording item)
+        {
+            var old = Read(item.Id);
+            old.SampleRate = item.SampleRate;
+            old.SamplesJson = item.SamplesJson;
+        }
+    }
+}
