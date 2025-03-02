369d1cbc1d833828bba4a2ced71069538082e202
Oláh Levente <145545651+Dev-Levy@users.noreply.github.com>
2025-03-02 22:08:15 +0100

    3-create-repository-for-recordings

--- SheetMusicMaker/Frontend/Client.CLI/Program.cs
+++ SheetMusicMaker/Frontend/Client.CLI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Models;
 using Repository;
+using Repository.ModelRepos;
 
 namespace Client.CLI
 {
@@ -10,7 +11,10 @@ namespace Client.CLI
             var recording1 = new Recording() { SampleRate = 44100, SamplesJson = """[-0.7215260011951632, -0.07588045874281213, 0.21186210277807715, -0.040237382572468006, 0.9971275226378027, 0.49393531705285687, 0.3294170701450472, 0.13894373490541412, 0.6082752365243496, 0.3672959071141233]""" };
 
             SheetMusicMakerDBContext ctx = new();
-            ctx.Recordings.Add(recording1);
+            RecordingRepository recRepo = new(ctx);
+            recRepo.Create(recording1);
+
+            var rec1 = recRepo.Read(id: 1);
         }
     }
 }
