23bee4e917bb07a4f79cc446126b09fcb86b451a
Oláh Levente <levente@stud.uni-obuda.hu>
2025-03-02 22:14:05 +0100

    created pdf repo

--- SheetMusicMaker/Frontend/Client.CLI/Program.cs
+++ SheetMusicMaker/Frontend/Client.CLI/Program.cs
@@ -9,12 +9,17 @@ namespace Client.CLI
         static void Main(string[] args)
         {
             var recording1 = new Recording() { SampleRate = 44100, SamplesJson = """[-0.7215260011951632, -0.07588045874281213, 0.21186210277807715, -0.040237382572468006, 0.9971275226378027, 0.49393531705285687, 0.3294170701450472, 0.13894373490541412, 0.6082752365243496, 0.3672959071141233]""" };
+            var pdf1 = new Pdf() { Name = "asd.pdf", Content = [], CreatedAt = new System.DateTime(2003, 10, 16) };
 
             SheetMusicMakerDBContext ctx = new();
             RecordingRepository recRepo = new(ctx);
+            PdfRepository pdfRepo = new(ctx);
+
             recRepo.Create(recording1);
+            pdfRepo.Create(pdf1);
 
             var rec1 = recRepo.Read(id: 1);
+            var sheet1 = pdfRepo.Read(id: 1);
         }
     }
 }
