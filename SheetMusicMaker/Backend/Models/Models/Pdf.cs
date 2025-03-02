c87175809dd67b2a735687246f2c5c67938b6971
Oláh Levente <145545651+Dev-Levy@users.noreply.github.com>
2025-03-02 21:55:41 +0100

    2-implement-a-general-repository

---
+++ SheetMusicMaker/Backend/Models/Models/Pdf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    public class Pdf
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required byte[] Content { get; set; }
+        public required DateTime CreatedAt { get; set; }
+    }
+}
