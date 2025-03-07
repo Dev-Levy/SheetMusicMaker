07ce9334fdb5004fad2f146039d0fc8f89848503
Oláh Levente <levente@stud.uni-obuda.hu>
2025-03-07 21:10:08 +0100

    code behind added

--- SheetMusicMaker/Frontend/Client.WEB/src/lib/CodeBehind.js
+++ SheetMusicMaker/Frontend/Client.WEB/src/lib/CodeBehind.js
@@ -1,37 +1,37 @@
 export async function UploadWav(file) {
-    const data = new FormData()
-    data.append('file', file)
+    const data = new FormData();
+    data.append("file", file);
 
     try {
-        const response = await fetch('http://localhost:5151/api/Recording', {
-            method: 'POST',
+        const response = await fetch("http://localhost:5151/api/Recording", {
+            method: "POST",
             body: data,
-        })
+        });
 
         if (response.ok) {
-            const result = await response.json()
-            console.log('Upload successful:', result)
+            const result = await response.json();
+            console.log("Upload successful:", result);
         } else {
-            console.error('Upload failed:', response.statusText)
+            console.error("Upload failed:", response.statusText);
         }
     } catch (error) {
-        console.error('Error uploading file:', error)
+        console.error("Error uploading file:", error);
     }
 }
 
 export async function DownloadPdf() {
     try {
-        const response = await fetch('http://localhost:5151/api/Recording', {
-            method: 'GET',
-        })
+        const response = await fetch("http://localhost:5151/api/Recording", {
+            method: "GET",
+        });
 
         if (response.ok) {
-            const result = await response.json()
-            console.log('Upload successful:', result)
+            const result = await response.json();
+            console.log("Upload successful:", result);
         } else {
-            console.error('Upload failed:', response.statusText)
+            console.error("Upload failed:", response.statusText);
         }
     } catch (error) {
-        console.error('Error uploading file:', error)
+        console.error("Error uploading file:", error);
     }
 }
