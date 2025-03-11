7e47df355ffbbb8c2d5d5842b4c678db1607beb5
Oláh Levente <levente@stud.uni-obuda.hu>
2025-03-11 20:19:22 +0100

    started making main page

--- SheetMusicMaker/Frontend/Client.WEB/src/App.svelte
+++ SheetMusicMaker/Frontend/Client.WEB/src/App.svelte
@@ -1,16 +1,28 @@
 <script>
-    let files = $state();
+    let files = $state(null);
     import {UploadWav, DownloadPdf} from './lib/CodeBehind';
+    import './lib/style.css'
 </script>
 
 <main>
-    <h1>Hej</h1>
+  <h1>Hej</h1>
 
     <h2>Try my sheet music maker app</h2>
-    <input type="file" bind:files={files} accept=".wav">
-    <button onclick={()=>UploadWav(files[0])}>Upload</button>
 
-    <h2>Try downloading your PDF</h2>
-    <p>PDF created!</p>
-    <button onclick={()=>DownloadPdf(2)}>Download</button>
+    <div class="container">
+        
+        <div class="column">
+            <input type="file" bind:files={files} accept=".wav"/>
+            <button disabled={!files}>Upload</button>
+        </div>
+    
+        <div class="column">
+            <button >Analyze</button>
+        </div>
+    
+        <div class="column">
+            <p>ASDASDASD</p>
+            <button>Download Result</button>
+        </div>
+    </div>
 </main>
